d1ad4856d5e87fe229a98a2e8dcd3c6f213aa09a
Lufajs <faronludwik@gmail.com>
2023-12-18 08:47:19 +0100

    0.2

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,52 +1,5 @@
 import NextAuth from "next-auth";
-import { Account, User as AuthUser } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
-import User from "@/models/User";
-import connectMongoDB from "@/libs/mongodb";
+import { authOptions } from "@/libs/authOptions";
 
-export const authOptions = {
-  providers: [
-    CredentialsProvider({
-      id: "credentials",
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        await connectMongoDB();
-        try {
-          const user = await User.findOne({ email: credentials.email });
-
-          if (user) {
-            const isPasswordCorrect = await bcrypt.compare(
-              credentials.password,
-              user.password
-            );
-            if (isPasswordCorrect) {
-              return user;
-            }
-          }
-        } catch (error) {
-          throw new Error(error);
-        }
-      },
-    }),
-  ],
-  callbacks: {
-    async session({ session }) {
-      const user = await User.findOne({
-        email: session.user.email,
-      });
-
-      const { password, ...userData } = user.toObject();
-      session.user = userData;
-
-      return session;
-    },
-  },
-};
-
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
