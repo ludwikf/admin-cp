162fae17ef086b52454b94b2bbc301ef0de9322d
Lufajs <faronludwik@gmail.com>
2024-01-29 08:37:47 +0100

    .

--- app/ui/dashboard/User.tsx
+++ app/ui/dashboard/User.tsx
@@ -19,42 +19,28 @@ async function fetchUser() {
 }
 
 export default async function User({ dict }: any) {
-  // const fetchUser = async () => {
-  //   try {
-  //     const res = await fetch(
-  //       `${process.env.NEXT_PUBLIC_WEB_URL}/api/get-user-new`
-  //     );
-
-  //     if (!res.ok) {
-  //       throw new Error("Failed to fetch data");
-  //     }
-
-  //     return res.json();
-  //   } catch (error) {
-  //     return null;
-  //   }
-  // };
-
   const user = await fetchUser();
   return (
     <div className="w-[80%] h-[70%] flex flex-col items-center">
-      <div className="flex flex-col items-center xs:block">
-        <div className="text-xl short:text-xl lg:text-2xl mb-3 xs:mb-6">
-          {dict.user.title}
-        </div>
-        <div className="flex items-center mb-6 gap-2">
-          <div>
-            <UserCircleIcon className="w-16 mt-1" />
+      {user && user[0] && (
+        <div className="flex flex-col items-center xs:block">
+          <div className="text-xl short:text-xl lg:text-2xl mb-3 xs:mb-6">
+            {dict.user.title}
           </div>
-          <div>
-            <div className="text-xl xs:text-2xl text-mainTheme mb-1">
-              {user[0]?.username}
+          <div className="flex items-center mb-6 gap-2">
+            <div>
+              <UserCircleIcon className="w-16 mt-1" />
             </div>
-            <div className="text-sm xs:text-md">{user[0]?.email}</div>
+            <div>
+              <div className="text-xl xs:text-2xl text-mainTheme mb-1">
+                {user[0]?.username}
+              </div>
+              <div className="text-sm xs:text-md">{user[0]?.email}</div>
+            </div>
           </div>
+          <div>{user[0]?.createdAt && FormatDate(user[0].createdAt, dict)}</div>
         </div>
-        <div>{user[0]?.createdAt && FormatDate(user[0].createdAt, dict)}</div>
-      </div>
+      )}
     </div>
   );
 }
